0920a7c8b0e80cdbb439998386bf4921ba31349b
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-10-06 16:30:01 +0800

    create server

---
+++ src/http-server01.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    response.end('<h2>Hi server</h2>')
+})
+
+server.listen(5000);
