d34ca49422f3877c178e291ce7f3f72e216da57e
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-10-06 16:43:29 +0800

    add request.url

--- src/http-server01.js
+++ src/http-server01.js
@@ -1,10 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, {
+    response.writeHead(200, { 
+        //status code為避田
         'Content-Type': 'text/html'
     });
-    response.end('<h2>Hi server</h2>')
+    response.end(`<h1>${request.url}</h1>`);
 })
 
 server.listen(5000);
+// 不要用1024以下的port，通常用3000/5000
+//用nodemon監測程式碼，只要有修改會自動重啟server
