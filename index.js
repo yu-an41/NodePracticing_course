f2c1880f7513a1ecf051bb4f56193ddbda547b16
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-10-11 09:52:14 +0800

    add 404 page

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
+require('dotenv').config();
+// dotenv會自動找到專案中最外層.env設定檔
+
 const express = require('express');
 
 const app = express();
 
-app.get('/', function (req, res) {
+app.use(express.static('public'));
+
+app.get('/', (req, res)  => {
     res.send('Hello world');
 });
 
-app.listen(5000, function () {
-    console.log('啟動server，埠號5000');
+const port = process.env.SERVER_PORT || 3002;
+// 若是沒有找到就會用3002當作通訊埠
+
+app.get('/abc', function(req, res){
+    res.send(`<h2>倪好~~</h2>`);
+})
+
+app.use((req, res) => {
+    res.type('text/html');
+    res.status(404);
+    res.send(`<h4  >There's nothing here!</h4>`);
+})
+
+app.listen(port, () => {
+    console.log(`server start, port: ${port}`);
 })
