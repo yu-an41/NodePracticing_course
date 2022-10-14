29436df36150f1229ea32a89de16ad7783b69ab4
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-10-14 16:57:30 +0800

    add sql

--- routes/address-book.js
+++ routes/address-book.js
@@ -17,15 +17,20 @@ router.get('/', async (req, res) => {
     const [[{totalRows}]] = await db.query(t_sql);
 
     let totalPages = 0;
+    let rows = [];
+
     if(totalRows>0) {
         totalPages = Math.ceil(totalRows/perPage);
         if(page > totalPages) {
             return res.redirect(`?page=${totalPages}`);
         }
+        const sql = `SELECT * FROM address_book ORDER BY sid DESC LIMIT ${(perPage - 1) * perPage}, ${perPage}`;
 
+        [rows] = await db.query(sql);
+        //因為rows已經在外面宣告過，可以用[]的方式找到並重新給值
     }
 
-    res.json({totalRows, totalPages, perPage, page});
+    res.json({totalRows, totalPages, perPage, page, rows});
 })
 
 module.exports = router;
