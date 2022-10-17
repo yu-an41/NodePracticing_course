32e5f3de74c354c3cf4aa1b7d36393201bca600c
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-10-17 11:05:06 +0800

    add name search

--- routes/address-book.js
+++ routes/address-book.js
@@ -13,7 +13,16 @@ router.get(['/', '/list'], async (req, res) => {
         return res.redirect(req.baseUrl);
     }
 
-    const t_sql = "SELECT COUNT(1) totalRows FROM address_book";
+    let search = req.query.search? req.query.search.trim(): ''; 
+    let where = ` WHERE 1 `;
+    if(search) {
+        where += ` AND \`name\` LIKE ${db.escape('%'+search+'%')} `;
+    }
+
+    // res.type('text/plain, charset=utf-8');
+    // return res.end(where);
+
+    const t_sql = `SELECT COUNT(1) totalRows FROM address_book ${where}`;
     const [[{totalRows}]] = await db.query(t_sql);
 
     let totalPages = 0;
@@ -24,14 +33,14 @@ router.get(['/', '/list'], async (req, res) => {
         if(page > totalPages) {
             return res.redirect(`?page=${totalPages}`);
         }
-        const sql = `SELECT * FROM address_book ORDER BY sid DESC LIMIT ${(perPage - 1) * perPage}, ${perPage}`;
+        const sql = `SELECT * FROM address_book ORDER BY sid DESC LIMIT ${(page - 1) * perPage}, ${perPage}`;
 
         [rows] = await db.query(sql);
         //因為rows已經在外面宣告過，可以用[]的方式找到並重新給值
     }
 
     // res.json({totalRows, totalPages, perPage, page, rows});
-    
+
     res.render('address-book/list', {totalRows, totalPages, perPage, page, rows});
 })
 
